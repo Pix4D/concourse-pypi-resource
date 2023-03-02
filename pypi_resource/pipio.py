b335d567907447d0e1e4728f3cd83c3aa959bbd6
Andrea Cristalli <andrea.cristalli@pix4d.com>
2023-03-02 17:47:35 +0100

    WIP

--- pypi_resource/pipio.py
+++ pypi_resource/pipio.py
@@ -129,8 +129,7 @@ def get_pypi_url(input, mode='in', kind='repository') -> Tuple[str, str]:
             url_parts[1]
         )
         host_login = host_login.lstrip(':@')
-        #url = urlunsplit((url_parts[0], host_login, url_parts[2], url_parts[3], url_parts[4]))
-        url = f"{url_parts.scheme}://{host_login}{url_parts.netloc}{url_parts.path}"
+        url = urlunsplit((url_parts[0], host_login, url_parts[2], url_parts[3], url_parts[4]))
 
     hostname = url_parts[1].split(':')[0]
     return url, hostname
